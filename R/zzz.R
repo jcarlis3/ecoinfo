477b1cf3fcfc833c729b70aaca118457137cf22a
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-06-19 12:26:53 -0600

    Add zzz and update description

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname){
+
+  # Manually update the version number
+  v <- "0.1"
+
+  # Startup message
+  packageStartupMessage(paste("jdcR (version ", v ,")", sep=""))
+
+}
